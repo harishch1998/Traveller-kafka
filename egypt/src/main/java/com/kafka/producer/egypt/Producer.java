f1b435d90169b214090042a5ee32fd6be5382714
Harish Chandrasekaran <harishchandrasekaran@Harishs-MacBook-Pro.local>
2021-12-10 19:13:55 -0500

    Changing producers to use Kafka Producers

--- egypt/src/main/java/com/kafka/producer/egypt/Producer.java
+++ egypt/src/main/java/com/kafka/producer/egypt/Producer.java
@@ -1,17 +1,32 @@
 package com.kafka.producer.egypt;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
+
 @Service
 public class Producer {
-    //One
     //This is a producer for topic Egypt
     private static final String TOPIC = "egypt";
-    @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaProducer producer = getProducer();
+    public KafkaProducer getProducer() {
+        Properties producerProperties = new Properties();
+        producerProperties.put("bootstrap.servers", "kafka1:9092, kafka2:9092, kafka3:9092");
+        producerProperties.put("acks", "all");
+        producerProperties.put("retries", 0);
+        producerProperties.put("batch.size", 16384);
+        producerProperties.put("linger.ms", 1);
+        producerProperties.put("buffer.memory", 33554432);
+        producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
+        return producer;
+    }
     public void sendMessage(String message) {
-        this.kafkaTemplate.send(TOPIC, message);
+        producer.send(new ProducerRecord<>(TOPIC, message));
     }
 }
